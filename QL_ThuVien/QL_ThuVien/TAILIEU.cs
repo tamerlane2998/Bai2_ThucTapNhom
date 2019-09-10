556eb34a78c8f51babfb31e6202f694a66616b80
DoVanHao <DoVanHao@users.noreply.github.com>
2019-09-10 04:36:23 -0700

    Thêm form tài liệu

---
+++ QL_ThuVien/QL_ThuVien/TAILIEU.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KetNoiDB;
+using BangThuVien;
+namespace QL_ThuVien
+{
+    public partial class frmTAILIEU : Form
+    {
+        public frmTAILIEU()
+        {
+            InitializeComponent();
+        }
+        
+
+       
+    }
+}
