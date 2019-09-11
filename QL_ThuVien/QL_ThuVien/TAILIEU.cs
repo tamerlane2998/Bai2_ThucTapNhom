45cdd9b47088cbf930430a6534f97fde99b64855
DoVanHao <DoVanHao@users.noreply.github.com>
2019-09-11 15:23:36 -0700

    Thêm fuction điều khiển

--- QL_ThuVien/QL_ThuVien/TAILIEU.cs
+++ QL_ThuVien/QL_ThuVien/TAILIEU.cs
@@ -17,7 +17,36 @@ namespace QL_ThuVien
         {
             InitializeComponent();
         }
-        
+        BUS_TaiLieu _tailieu = new BUS_TaiLieu();
+        int chon = 0;
+        TimKiem tk = new TimKiem();
+
+        void KhoaDieuKhien()
+        {
+            txtTG.Enabled = txtNhanDe.Enabled = txtSoLuong.Enabled = txtNgonNgu.Enabled = cbMaTheLoai.Enabled = cbMaNXB.Enabled = txtDoMat.Enabled = false;
+            btnThem.Enabled = btnSua.Enabled = btnXoa.Enabled = true;
+            btnLuu.Enabled = false;
+            
+        }
+
+        void MoDieuKhien()
+        {
+            txtTG.Enabled = txtNhanDe.Enabled = txtSoLuong.Enabled = txtNgonNgu.Enabled = cbMaTheLoai.Enabled = cbMaNXB.Enabled = txtDoMat.Enabled = true;
+            btnThem.Enabled = btnSua.Enabled = btnXoa.Enabled =false;
+            btnLuu.Enabled = true;
+        }
+
+        void SetNull()
+        {
+            txtDoMat.Text = cbMaNXB.Text = txtMaTL.Text = cbMaTheLoai.Text = txtNgonNgu.Text = txtNhanDe.Text = txtSoLuong.Text = txtTG.Text = "";
+            txttk_MaTL.Text = txttk_NhanDe.Text = txttk_Tacgia.Text = "";
+        }
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            MoDieuKhien();
+            SetNull();
+            chon = 1;
+        }
 
        
     }
