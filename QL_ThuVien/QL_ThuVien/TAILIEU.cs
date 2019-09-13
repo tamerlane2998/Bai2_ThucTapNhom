3ed818d511ab867def9e2cd167f22cddacea4d7d
DoVanHao <DoVanHao@users.noreply.github.com>
2019-09-12 22:25:36 -0700

    Thêm fuction sửa lưu

--- QL_ThuVien/QL_ThuVien/TAILIEU.cs
+++ QL_ThuVien/QL_ThuVien/TAILIEU.cs
@@ -47,6 +47,75 @@ namespace QL_ThuVien
             SetNull();
             chon = 1;
         }
+		
+		rivate void btnSua_Click(object sender, EventArgs e)
+        {
+            MoDieuKhien();
+            chon = 2;
+        }
+        private void frmTAILIEU_Load(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt = _tailieu.HienThiTaiLieu();
+            dgvTaiLieu.DataSource = dt;
+            KhoaDieuKhien();
+        }
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.Yes == MessageBox.Show("Bạn muốn xóa Tài liệu này?", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+            {
+                _tailieu.XoaTaiLieu(txtMaTL.Text);
+                MessageBox.Show("Xóa thành công!");
+               frmTAILIEU_Load(sender,e);
+                SetNull();
+            }
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (chon == 1)
+            {
+                if (txtNhanDe.Text == "" || txtTG.Text == "" || txtSoLuong.Text == "" || cbMaTheLoai.Text == "" || cbMaNXB.Text == "" || txtDoMat.Text == "" || txtNgonNgu.Text == "")
+                    MessageBox.Show("Mời nhập đầy đủ thông tin!");
+                else
+                    if (DialogResult.Yes == MessageBox.Show("Bạn có muốn thêm nhân viên này?", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                    {
+                        _tailieu.ThemTaiLieu(txtTG.Text, txtNhanDe.Text, int.Parse(txtSoLuong.Text), int.Parse(txtDoMat.Text), txtNgonNgu.Text, cbMaTheLoai.Text, cbMaNXB.Text);
+                        MessageBox.Show("Thêm thành công!");
+                        SetNull();
+                        frmTAILIEU_Load(sender, e);
+                    }
+            }
+            else if (chon == 2)
+            {
+                if (txtNhanDe.Text == "" || txtTG.Text == "" || txtSoLuong.Text == "" || cbMaTheLoai.Text == "" || cbMaNXB.Text == "" || txtDoMat.Text == "" || txtNgonNgu.Text == "")
+                    MessageBox.Show("Mời nhập đầy đủ thông tin!");
+                else
+                    if (DialogResult.Yes == MessageBox.Show("Bạn có muốn Sửa nhân viên này?", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+                    {
+                        _tailieu.SuaTaiLieu(txtMaTL.Text,txtTG.Text, txtNhanDe.Text, int.Parse(txtSoLuong.Text), int.Parse(txtDoMat.Text), txtNgonNgu.Text, cbMaTheLoai.Text, cbMaNXB.Text);
+                        MessageBox.Show("Sửa thành công!");
+                        SetNull();
+                        frmTAILIEU_Load(sender, e);
+                    }
+            }
+        }
+
+        private void dgvNhanVien_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                txtMaTL.Text = dgvTaiLieu.Rows[e.RowIndex].Cells[0].Value.ToString();
+                txtTG.Text = dgvTaiLieu.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtNhanDe.Text = dgvTaiLieu.Rows[e.RowIndex].Cells[2].Value.ToString();
+                txtSoLuong.Text = dgvTaiLieu.Rows[e.RowIndex].Cells[3].Value.ToString();
+                txtDoMat.Text = dgvTaiLieu.Rows[e.RowIndex].Cells[4].Value.ToString();
+                txtNgonNgu.Text = dgvTaiLieu.Rows[e.RowIndex].Cells[5].Value.ToString();
+                cbMaTheLoai.Text = dgvTaiLieu.Rows[e.RowIndex].Cells[6].Value.ToString();
+                cbMaNXB.Text = dgvTaiLieu.Rows[e.RowIndex].Cells[7].Value.ToString();
+            }
+            catch { }
+        }
 
        
     }
