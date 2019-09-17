1e096d5788a6c1c4ffbfc2c76eeec27035f51d69
tamerlane2998 <trieuquangminh29@gmail.com>
2019-09-17 20:01:37 +0700

    KetnoiCSDL

---
+++ QL_ThuVien/KetNoi/KetNoi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace KetNoi
+{
+    public class KetNoi
+    {
+        SqlConnection cn = new SqlConnection();
+        public static String connect()
+        {
+            return (@"Data Source=NHOXHOANGANH\SQLEXPRESS;Initial Catalog=QL_ThuVien;Integrated Security=true;");
+        }
+    }
+}
